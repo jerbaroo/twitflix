6a9fe96db4536a515217117138e59a0f629a1dbe
Bas van der Borden <bascasper@gmail.com>
2019-03-12 14:25:04 +0100

    class update + pep8

--- Sentiment.py
+++ Sentiment.py
@@ -1,33 +1,34 @@
-import sys, os
+import sys
+import os
 import json
 from nltk import tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-def main(argv):
-# import the tweet file
-    if len(argv) == 0:
-        print('input the file for sentiment analysis')
-        return
 
-    with open(argv[0]) as f:
-        data = json.load(f)
+class sentiment:
+    """docstring for sentiment."""
 
-# run vader over the tweet text and return scores
-    text = []
-    sentences = []
-    for entry in data:
-        text.append(data[entry]['Text'])
-    for paragraph in text:
-        lines_list = tokenize.sent_tokenize(paragraph)
-        sentences.extend(lines_list)
-    sid = SentimentIntensityAnalyzer()
-    for sentence in sentences:
-        print(sentence)
-        ss = sid.polarity_scores(sentence)
-        for k in sorted(ss):
-            print('{0}: {1}, '.format(k, ss[k]))
-        print()
+    def __init__(self):
+        pass
 
+    # Sentiment analysis of tweets
+    def Analysis(self, file):
+        # import the tweet file
+        with open(file) as f:
+            data = json.load(f)
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+        # run vader over the tweet text and return scores
+        text = []
+        sentences = []
+        for entry in data:
+            text.append(data[entry]['Text'])
+        for paragraph in text:
+            lines_list = tokenize.sent_tokenize(paragraph)
+            sentences.extend(lines_list)
+        sid = SentimentIntensityAnalyzer()
+        for sentence in sentences:
+            print(sentence)
+            ss = sid.polarity_scores(sentence)
+            for k in sorted(ss):
+                print('{0}: {1}, '.format(k, ss[k]))
+            print()
--- Tweets.py
+++ Tweets.py
@@ -1,5 +1,8 @@
 # Imports
-import sys, os, datetime, json
+import sys
+import os
+import datetime
+import json
 
 # check for version of python
 if sys.version_info[0] < 3:
@@ -7,61 +10,60 @@ if sys.version_info[0] < 3:
 else:
     from GetOldTweets import got3 as got
 
+
 # converter to include datetime into json output file
 def datetimeConverter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
 
-# function to save to json file
-def saveJSON(title, tweets):
-    print(title)
-    data = {}
-    for tweet in tweets:
-        # print(tweet.username)
-        data[tweet.id] = {
-            "Title": title,
-            "Username": tweet.username,
-            "Text": tweet.text,
-            "Mentions": tweet.mentions,
-            "Hashtags": tweet.hashtags,
-            "Date": tweet.date,
-            "Geo": tweet.geo
-        }
-    with open("Movies/%s.json" % title, "w") as outfile:
-        json.dump(data, outfile, default=datetimeConverter)
 
+class data:
+    """docstring for data."""
+    def __init__(self):
+        pass
 
-def main(argv):
-    if not os.path.isdir('Movies'):
-        try:
-            os.mkdir('Movies')
-        except OSError:
-            print("Creation of the directory Movies failed")
-        else:
-            print ("Successfully created the directory Movies")
+    # function to save to json file
+    def SaveJSON(self, title, tweets):
+        print(title)
+        data = {}
+        for tweet in tweets:
+            # print(tweet.username)
+            data[tweet.id] = {
+                "Title": title,
+                "Username": tweet.username,
+                "Text": tweet.text,
+                "Mentions": tweet.mentions,
+                "Hashtags": tweet.hashtags,
+                "Date": tweet.date,
+                "Geo": tweet.geo
+            }
+        with open("Movies/%s.json" % title, "w") as outfile:
+            json.dump(data, outfile, default=datetimeConverter, encoding="utf8")
 
-    if len(argv) == 0:
-        print("enter the number of tweets per title")
-        return
+    # Gather tweets about movies
+    def Gather(self, file, tweets):
+        if not os.path.isdir('Movies'):
+            try:
+                os.mkdir('Movies')
+            except OSError:
+                print("Creation of the directory Movies failed")
+            else:
+                print ("Successfully created the directory Movies")
 
-    n = int(argv[0])
-    print(n)
-# open netflix-media list
-    with open('netflix-media.json') as f:
-        movies = json.load(f)
-# iterate over the different titles and save output
-    for title in movies:
-        tweetCriteria = got.manager.TweetCriteria().setQuerySearch(title).setMaxTweets(n)
-        tweet = got.manager.TweetManager.getTweets(tweetCriteria)
-        saveJSON(title, tweet)
-        query = title + " #movierating"
-        tweetCriteria = got.manager.TweetCriteria().setQuerySearch(query).setMaxTweets(n)
-        tweet = got.manager.TweetManager.getTweets(tweetCriteria)
-        saveJSON(title+"#movierating", tweet)
-        query = title + " #rating"
-        tweetCriteria = got.manager.TweetCriteria().setQuerySearch(query).setMaxTweets(n)
-        tweet = got.manager.TweetManager.getTweets(tweetCriteria)
-        saveJSON(title+"#rating", tweet)
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+        n = int(tweets)
+    # open netflix-media list
+        with open(file) as f:
+            movies = json.load(f)
+    # iterate over the different titles and save output
+        for title in movies:
+            tweetCriteria = got.manager.TweetCriteria().setQuerySearch(title).setMaxTweets(n)
+            tweet = got.manager.TweetManager.getTweets(tweetCriteria)
+            self.SaveJSON(title, tweet)
+            query = title + " #movierating"
+            tweetCriteria = got.manager.TweetCriteria().setQuerySearch(query).setMaxTweets(n)
+            tweet = got.manager.TweetManager.getTweets(tweetCriteria)
+            self.SaveJSON(title+"#movierating", tweet)
+            query = title + " #rating"
+            tweetCriteria = got.manager.TweetCriteria().setQuerySearch(query).setMaxTweets(n)
+            tweet = got.manager.TweetManager.getTweets(tweetCriteria)
+            self.SaveJSON(title+"#rating", tweet)
