0bea48cc536c088380df6b73ddb6dc55c16810d7
Bas van der Borden <bascasper@gmail.com>
2019-03-15 16:07:55 +0100

    output all results to results.json

--- omdb.py
+++ omdb.py
@@ -3,6 +3,7 @@
 import requests
 import json
 
+
 class Omdb(object):
     """docstring for Omdb."""
     def __init__(self):
@@ -11,36 +12,56 @@ class Omdb(object):
         self.rotten_tomatoes = {}
         self.metacritic = {}
         self.missing = 0
+        self.genre = 0
 
     def critic_scores(self, file):
         key = '289bb0f9'
         url = 'http://www.omdbapi.com/?apikey=289bb0f9&t='
         with open(file) as f:
             movies = json.load(f)
+        result = {}
 
         for title in movies:
+            data = {}
+            result[title] = {
+                "genre": "",
+                "imdb": 0,
+                "Rotten Tomatoes": 0,
+                "Metacritic": 0
+            }
             url = 'http://www.omdbapi.com/?apikey=289bb0f9&t='
             url = url+title
-            print(url)
             response = requests.post(url)
             response = response.json()
-            print(response)
             self.response = response
             try:
+                genre = response['Genre']
+                data['genre'] = genre
+                result[title]['genre'] = genre
+            except KeyError:
+                self.genre += 1
+
+            try:
                 ratings = response['Ratings']
-                data = {}
                 for platform in ratings:
                     if platform['Source'] == 'Internet Movie Database':
                         self.imdb[title] = platform['Value']
                         data['imdb'] = platform['Value']
+                        result[title]['imdb'] = platform['Value']
                     elif platform['Source'] == 'Rotten Tomatoes':
                         self.rotten_tomatoes[title] = platform['Value']
                         data['Rotten Tomatoes'] = platform['Value']
+                        result[title]['Rotten Tomatoes'] = platform['Value']
                     elif platform['Source'] == 'Metacritic':
                         self.metacritic[title] = platform['Value']
                         data['Metacritic'] = platform['Value']
+                        result[title]['Metacritic'] = platform['Value']
 
                 with open('Results/%s.json' % title, "w") as outfile:
                     json.dump(data, outfile, indent=1)
             except KeyError:
+                print(title)
                 self.missing += 1
+
+        with open('Results/results.json', "w") as target:
+            json.dump(result, target, indent=1)
